c54475b562290154e09d9883467652b1e4636b34
VijayAbba <vijaykrishna.ed@gmail.com>
2024-06-16 20:17:22 +0530

    174. Rendering a Map with Google , Nominatim Maps (incl. Types!)

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import axios from "axios";
 
+declare const L: any;
+const ZOOM = 13;
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-type ResponseType = {
-  lat: string;
-  lon: string;
-}[];
+const map = L.map("map");
+
+type NominatimGeocodingResponse = {
+  features: {
+    geometry: { coordinates: { lat: Number; lng: Number }[] };
+    properties: { geocoding: { city: String; country: String; postcode: String } };
+  }[];
+  status: 200;
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
@@ -15,17 +23,41 @@ function searchAddressHandler(event: Event) {
   // send this to Google's API!
 
   axios
-    .get<ResponseType>(
-      `https://nominatim.openstreetmap.org/search?q=${encodeURI(enteredAddress)}&format=json`
+    .get<NominatimGeocodingResponse>(
+      `https://nominatim.openstreetmap.org/?addressdetails=1&q=${encodeURI(
+        enteredAddress
+      )}&format=geocodejson&limit=1`
     )
     .then((response) => {
       console.log(response);
-      if (response.status !== 200) {
+      console.log(response.data);
+      console.log(response.data.features);
+      if (!response.data.features.length) {
         throw new Error("Could not fetch location!");
       }
 
-      const coordinates = { lat: response.data[0].lat, lon: response.data[0].lon };
-      console.log(coordinates);
+      const coordinates = [
+        response.data.features[0].geometry.coordinates[1],
+        response.data.features[0].geometry.coordinates[0],
+      ];
+
+      const location = {
+        city: response.data.features[0].properties.geocoding.city,
+        country: response.data.features[0].properties.geocoding.country,
+        postcode: response.data.features[0].properties.geocoding.postcode,
+      };
+
+      map.setView(coordinates, ZOOM);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coordinates)
+        .addTo(map)
+        .bindPopup(`${location.city}<br>${location.country}<br>${location.postcode}`)
+        .openPopup();
     })
     .catch((err) => {
       alert(err.message);
